afc3b397fdf234338cbda12f2ddcc2ce45cdd284
chavantejaswini <chavan.t@northeastern.edu>
2025-04-16 01:17:13 -0400

    Testing 02

--- infra/packer/userdata.sh
+++ infra/packer/userdata.sh
@@ -9,7 +9,7 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 
-# ---------------------
+# ---------------------f
 # Install Node.js
 # ---------------------
 echo "Installing Node.js runtime..."
