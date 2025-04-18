c7e5d9e011b1bc3fdbda68213c77f978f3301405
chavantejaswini <chavan.t@northeastern.edu>
2025-04-18 02:43:55 -0400

    testing payment issue

--- models/file.js
+++ models/file.js
@@ -17,14 +17,6 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
         field: "file_name",
       },
-
-
-
-
-
-
-
-
       
       // URL where the file is stored (typically on S3)
       url: {
